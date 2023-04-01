8d44171c09ae0a757ffc4cd2e44459cd739b202d
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2023-04-02 02:11:02 +0500

    todo list changes

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,109 +1,100 @@
 import React, { useState } from "react";
-import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { IoIosArrowDropdown, IoIosArrowDropup } from "react-icons/io";
 
-interface Subtask {
+type Subtask = {
   name: string;
   description: string;
-  isDone: boolean;
-}
+  done: boolean;
+};
 
-interface Task {
-  task_name: string;
-  subtasks: Subtask[];
-}
+type Props = {
+  data: {
+    task_name: string;
+    subtasks: Subtask[];
+  };
+};
 
-interface Props {
-  task: Task;
-}
-
-const TodoList: React.FC<Props> = ({ task }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const [isTaskDone, setIsTaskDone] = useState(false);
-  const [subtaskStatus, setSubtaskStatus] = useState(
-    task.subtasks.every((subtask) => subtask.isDone)
+const TodoList: React.FC<Props> = ({ data }) => {
+  const [subtasks, setSubtasks] = useState<Subtask[]>(data.subtasks);
+  const [isDone, setIsDone] = useState<boolean>(
+    data.subtasks.every((subtask) => subtask.done)
   );
+  const [showSubtasks, setShowSubtasks] = useState<boolean>(false);
 
-  const handleToggle = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  const handleTaskToggle = () => {
-    const updatedSubtasks = task.subtasks.map((subtask) => ({
+  const handleMainTaskChange = () => {
+    const updatedSubtasks = subtasks.map((subtask) => ({
       ...subtask,
-      isDone: !subtaskStatus,
+      done: !isDone,
     }));
-    const updatedTask = { ...task, subtasks: updatedSubtasks };
-    setIsTaskDone(!isTaskDone);
-    setSubtaskStatus(!subtaskStatus);
-
-    // Call a function to update the task with the new subtask list
-    updateTask(updatedTask);
-  };
-  const handleSubtaskToggle = (index: number) => {
-    const updatedSubtasks = [...task.subtasks];
-    updatedSubtasks[index].isDone = !updatedSubtasks[index].isDone;
-
-    const isTaskDone = updatedSubtasks.every((subtask) => subtask.isDone);
-    setIsTaskDone(isTaskDone);
-    setSubtaskStatus(isTaskDone);
-
-    // Update the task with the new subtask list
-    const updatedTask = {
-      ...task,
-      subtasks: updatedSubtasks,
-    };
-    // Call a function to update the task in your state or database
-    updateTask(updatedTask);
+    setSubtasks(updatedSubtasks);
+    setIsDone(!isDone);
+    const taskElement = document.getElementById("main-task");
+    if (taskElement) {
+      taskElement.innerText = isDone
+        ? data.task_name
+        : `${data.task_name} - Mark as Done`;
+    }
   };
 
-  const updateTask = (updatedTask: Task) => {
-    // Implement a function to update the task in your state or database
-    console.log("Task updated:", updatedTask);
+  const handleSubtaskChange = (index: number) => {
+    const updatedSubtasks = [...subtasks];
+    updatedSubtasks[index].done = !updatedSubtasks[index].done;
+    setSubtasks(updatedSubtasks);
+    setIsDone(updatedSubtasks.every((subtask) => subtask.done));
   };
 
   return (
-    <div className="rounded-2xl bg-white border p-6 mt-16 flex flex-col h-[calc(100vh-6rem)]">
-      <div className="flex items-center justify-between mb-4">
-        <span className="flex space-x-2">
-          <p>{task.task_name}</p>
-          <span>
-            <IoIosArrowDropdownCircle
-              onClick={handleToggle}
-              className="cursor-pointer text-2xl"
-            />
+    <div className="rounded-2xl bg-white0 p-6 mt-5 flex flex-col">
+      <div className="font-bold text-gray-800 text-lg flex items-center justify-between">
+        <div className="flex space-x-2">
+          <span
+            className={isDone ? "line-through text-gray-500" : "text-gray-800"}
+          >
+            {data.task_name}
           </span>
-          {isTaskDone && (
-            <span className="text-green-500 font-bold">(Done)</span>
-          )}
-        </span>
-        <label>
+          <div className="flex text-2xl">
+            {showSubtasks ? (
+              <IoIosArrowDropup
+                className="text-gray-500 cursor-pointer"
+                onClick={() => setShowSubtasks(false)}
+              />
+            ) : (
+              <IoIosArrowDropdown
+                className="text-gray-500 cursor-pointer"
+                onClick={() => setShowSubtasks(true)}
+              />
+            )}
+          </div>
+        </div>
+        <div className="flex">
+          <p className="font-thin mr-2">
+            {isDone ? "Mark as Incomplete" : "Mark as Complete"}
+          </p>
           <input
             type="checkbox"
-            checked={isTaskDone}
-            onChange={handleTaskToggle}
-            className="mr-2"
+            className="m-2"
+            checked={isDone}
+            onChange={handleMainTaskChange}
           />
-          <span>Mark as Done</span>
-        </label>
+        </div>
       </div>
-      <hr></hr>
-      {isExpanded && (
-        <ul key={Date.now()}>
-          {task.subtasks.map((subtask, index) => (
-            <li
-              key={subtask.name}
-              className={`${subtask.isDone ? "line-through" : ""}`}
-            >
-              <label>
-                <input
-                  type="checkbox"
-                  checked={subtask.isDone}
-                  onChange={() => handleSubtaskToggle(index)}
-                  className="mr-2"
-                />
-                <span>{subtask.name}</span>
-              </label>
-              <p>{subtask.description}</p>
+      {showSubtasks && (
+        <ul className="list-disc list-inside mt-4">
+          {subtasks.map((subtask, index) => (
+            <li key={index} className="flex items-center">
+              <input
+                type="checkbox"
+                className="mr-3"
+                checked={subtask.done}
+                onChange={() => handleSubtaskChange(index)}
+              />
+              <span
+                className={
+                  subtask.done ? "line-through text-gray-500" : "text-gray-800"
+                }
+              >
+                {subtask.name}
+              </span>
             </li>
           ))}
         </ul>
