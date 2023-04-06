24445d213f0745a27b37d5e4a4cd03564c956492
Moosa Butt <moosabutt715@gmail.com>
2023-04-06 14:29:58 +0500

    linting fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { LayoutProps } from "@vercel/examples-ui/layout";
 import { getLayout } from "@vercel/examples-ui";
 
 import "@vercel/examples-ui/globals.css";
-import Header from "../components/Header";
 
 function App({ Component, pageProps }: AppProps) {
   const Layout = getLayout<LayoutProps>(Component);
